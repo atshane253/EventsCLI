6389b185e3f8fd96b22d139be00e129b8d3940a2
shanehoman <shane.homan@gmail.com>
2018-05-18 09:30:44 -0700

    Add project files.

---
+++ EventsCLI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EventsCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = $"Events on {(args.Length > 0 ? args[0] : "localhost")}, press q to quit, x to print xml";
+            ConsoleKeyInfo key;
+            var watcher = args.Length > 0 ? new Watcher("*[System[Provider[@Name='ASP.NET 4.0.30319.0']]]", args[0]) : new Watcher();
+            watcher.StartWatch();
+            while (true)
+            {
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.X)
+                    watcher.WriteLastXML();
+                if (key.Key == ConsoleKey.Q || key.Key == ConsoleKey.Escape || (key.Key==ConsoleKey.F4 && key.Modifiers==ConsoleModifiers.Alt))
+                    break;
+            }
+        }
+    }
+}
---
+++ EventsCLI/Watcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Xml.Linq;
+
+namespace EventsCLI
+{
+    public class Watcher
+    {
+        EventLogQuery query;
+        EventLogWatcher watch;
+        EventRecord last;
+        const string queryString ="<QueryList><Query Id=\"0\" Path=\"Application\"><Select Path=\"Application\">*</Select><Select Path=\"System\">*</Select></Query></QueryList>";
+
+        public Watcher(string text = queryString)
+        {
+            query = new EventLogQuery("Application", PathType.LogName, text);
+            watch = new EventLogWatcher(query);
+        }
+
+        public Watcher(string text, string server)
+        {
+            query = new EventLogQuery("Application", PathType.LogName, text);
+            query.Session = new EventLogSession(server);
+            watch = new EventLogWatcher(query);
+        }
+
+        public void StartWatch()
+        {
+            watch.Enabled = true;
+            watch.EventRecordWritten += Watch_EventRecordWritten;
+        }
+
+        private void Watch_EventRecordWritten(object sender, EventRecordWrittenEventArgs e)
+        {
+            last = e.EventRecord;
+            Console.WriteLine(last.FormatDescription());
+        }
+
+        public void WriteLastXML()
+        {
+            if (last != null)
+                Console.WriteLine(XDocument.Parse(last.ToXml()));            
+        }
+    }
+}
